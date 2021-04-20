294019bcf6cb1419f78bbd33ecf721202213b0e6
tauchner <7229@htl.rennweg.at>
2021-04-20 14:32:43 +0200

    Totally working don't doubt it don't look at it this is fucking fine GOT IT?

--- src/index.php
+++ src/index.php
@@ -2,33 +2,14 @@
 
 namespace ViewaLasVegas;
 
+use TYPO3Fluid\Fluid\Core\Cache\SimpleFileCache;
+use TYPO3Fluid\Fluid\View\TemplateView;
+
+//require "../vendor/autoload.php";
+
 spl_autoload_register(function ($class) {
-
-  // project-specific namespace prefix
-  $prefix = 'ViewaLasVegas\\';
-
-  // base directory for the namespace prefix
-  $base_dir = __DIR__ . '/src/';
-
-  // does the class use the namespace prefix?
-  $len = strlen($prefix);
-  if (strncmp($prefix, $class, $len) !== 0) {
-    // no, move to the next registered autoloader
-    return;
-  }
-
-  // get the relative class name
-  $relative_class = substr($class, $len);
-
-  // replace the namespace prefix with the base directory, replace namespace
-  // separators with directory separators in the relative class name, append
-  // with .php
-  $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-
-  // if the file exists, require it
-  if (file_exists($file)) {
-    require $file;
-  }
+  if (strpos($class, 'ViewaLasVegas'))
+    var_dump($class);
 });
 
 function var_dump_pre($mixed = null)
@@ -77,8 +58,20 @@ $hotels = [
     "A stroßn in Wean", "1170 Wien"),
 ];
 
-$template = file_get_contents("template.html");
+$view = new TemplateView();
 
-$template = insertHotels($template, "{{ HOTELS }}", $hotels);
+$paths = $view->getTemplatePaths();
+$view->assignMultiple([
+  'asdf' => $hotels,
+]);
 
-echo $template;
+$view->setCache(new SimpleFileCache("/cache/"));
+
+$paths->setTemplatePathAndFilename("template.html");
+
+$output = $view->render();
+
+echo $output;
+//$paths->setTemplateRootPaths(array("/templates/"));
+//$paths->setTemplateRootPaths(array("/layouts/"));
+//$paths->setTemplateRootPaths(array("/layouts/"));
